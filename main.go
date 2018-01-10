0b1a3e1ffb88535c00eee069710130bb2b93a1a9
Johnny Vestergaard <jkv@unixcluster.dk>
2018-01-10 21:21:10 +0100

    rename to main

--- main.go
+++ main.go
@@ -10,15 +10,14 @@ func main() {
 	fmt.Printf("Running!\n")
 
 	packetMessageChannel := make(chan packetMessage)
-	sessionEndChannel := make(chan sessionEndMessage)
+	sessionChannel := make(chan sessionMessage)
 	pcapWriterChannel := make(chan sessionEntry)
 
 	var wg sync.WaitGroup
 	wg.Add(1)
-	go heraldingPoller(sessionEndChannel)
+	go heraldingPoller(sessionChannel)
 	go pcapWriter(pcapWriterChannel)
-	go sessionMaster(&wg, packetMessageChannel, sessionEndChannel, pcapWriterChannel)
+	go sessionMaster(&wg, packetMessageChannel, sessionChannel, pcapWriterChannel)
 	go packetDumper(packetMessageChannel)
 	wg.Wait()
-
 }
